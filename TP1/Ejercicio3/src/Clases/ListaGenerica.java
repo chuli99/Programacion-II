4036bc416db3ecd8a3c0d3330e85f2dacbcb0deb
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2023-04-14 11:22:19 -0300

    Lista Generica actualizada con requisitos del ejercicio

--- TP1/Ejercicio3/src/Clases/ListaGenerica.java
+++ TP1/Ejercicio3/src/Clases/ListaGenerica.java
@@ -1,2 +1,82 @@
-package Clases;public class ListaGenerica {
+package Clases;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EmptyStackException;
+
+public class ListaGenerica<T> {
+    private ArrayList<T> lista;
+
+    public ListaGenerica() {
+
+        lista = new ArrayList<T>();
+    }
+    public void agregar(T elemento){
+        lista.add(elemento);
+    }
+
+    public ArrayList<T>devolver_Lista(){
+
+        return lista;
+    }
+
+    public int tamanio(){
+
+        return lista.size();
+    }
+    //Metodo para agregar al principio
+    public void agregar_principio(T elemento){
+
+        lista.add(0,elemento);
+    }
+    //Metodo para ordenar lista
+    //public void ordenar_lista() {
+    //    Collections.sort(lista);
+    //}
+
+    //Metodo para desordenar la lista
+    public void desordenar_lista(){
+
+        Collections.shuffle(lista);
+    }
+
+    //Metodo para agregar en una posicion determinada
+    public void agregar_posicion(T elemento, int posicion) throws IndexOutOfBoundsException {
+        if (posicion > lista.size()) {
+            throw new IndexOutOfBoundsException("El indice indicado es mayor al rango de la lista o simplemente no pertenece");
+        }
+        lista.add(posicion, elemento);
+    }
+
+    //Metodo para Obtener elemento en una posicion determinada
+    public T obtener_posicion(int posicion)throws IndexOutOfBoundsException {
+        if (posicion > lista.size()){
+            throw new IndexOutOfBoundsException("El indice indicado es mayor al rango de la lista o simplemente no pertenece");
+        }
+        return lista.get(posicion);
+    }
+
+    //Metodo para obtener el primer elemento
+    public T obtener_primer_elemento()throws IndexOutOfBoundsException{
+        if(lista.isEmpty()){
+            throw new IndexOutOfBoundsException("Esta lista esta vacia");
+        }
+        return lista.get(0);
+    }
+
+    //Metodo para obtener el ultimo elemento
+    public T obtener_ultimo_elemento()throws IndexOutOfBoundsException{
+        if(lista.isEmpty()){
+            throw new IndexOutOfBoundsException("Esta lista esta vacia");
+        }
+        return lista.get(lista.size() - 1);
+    }
+
+    //Metodo que remueve objeto en una posicion en particular
+    public void remover_objeto(int posicion)throws IndexOutOfBoundsException{
+        if(posicion > lista.size()){
+            throw new IndexOutOfBoundsException("El indice indicado es mayor al rango de la lista o simplemente no pertenece");
+        }
+        lista.remove(posicion);
+    }
 }
---
+++ TP1/Ejercicio3/src/Main.java
@@ -0,0 +1,36 @@
+import Clases.Empleado;
+import Clases.ListaGenerica;
+
+import javax.sound.midi.Soundbank;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.arranque();
+    }
+    public void arranque(){
+        Empleado empleado = new Empleado("Lionel","Messi",60031,15);
+        Empleado empleado2 = new Empleado("Lionel","Messi",60031,15);
+        Empleado empleado3 = new Empleado("Javier","Mascherano",60032,10);
+        Empleado empleado4 = new Empleado("Eduardo","Gomez",60035,2);
+        Empleado empleado5 = new Empleado("Enzo","Ferandez",60036,3);
+        Empleado empleado6 = new Empleado("Dani","Balda",56039,10);
+        ListaGenerica<Empleado> lista = new ListaGenerica<>();
+        lista.agregar(empleado);
+        lista.agregar(empleado2);
+        lista.agregar(empleado3);
+        lista.agregar(empleado4);
+        lista.agregar(empleado5);
+        System.out.println(lista.devolver_Lista());
+        System.out.println(lista.tamanio());
+        //lista.ordenar_lista();
+        lista.desordenar_lista();
+        lista.agregar_posicion((empleado6),2);
+        System.out.println(lista.obtener_posicion(2));
+        System.out.println(lista.obtener_primer_elemento());
+        System.out.println(lista.obtener_ultimo_elemento());
+        lista.remover_objeto(3);
+        System.out.println(lista.devolver_Lista());
+    }
+
+}
