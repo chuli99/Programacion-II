f8b2fef903e0b42d70fb576f2c23070dec3cbf9a
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2023-04-14 12:43:10 -0300

    Ejercicio 4 finalizado

--- TP1/Ejercicio4/src/Main.java
+++ TP1/Ejercicio4/src/Main.java
@@ -1,12 +1,20 @@
-
-
-import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.List;
+import Figuras.*;
+import Interfaces.FiguraGeometrica;
 
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         main.arranque();
     }
-    public void arranque(){
-        Array<>
+    public void arranque() {
+        List<FiguraGeometrica> figuras = new ArrayList<>();
+        figuras.add(new Circulo(4));
+        figuras.add(new Cuadrado(4));
+        figuras.add(new Triangulo(2, 2, 3, 2, 3));
+    for (FiguraGeometrica figura :figuras){
+        System.out.println(figura);
+            }
+        }
     }
