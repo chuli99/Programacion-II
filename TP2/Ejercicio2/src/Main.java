4a1cae49272cfb729da9ea4d53f643d5c9cd79b4
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2023-05-23 11:35:45 -0300

    Modifico Main de ejercicio 2

--- TP2/Ejercicio2/src/Main.java
+++ TP2/Ejercicio2/src/Main.java
@@ -4,33 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Main main = new Main();
-        main.arranque();
-    }
+        public static void main(String[] args){
+            Main main = new Main();
+            main.arranque();
+        }
 
     public void arranque(){
         List<Integer> numeros = new ArrayList<>();
         for (int i = 0; i < 40; i++) {
-            int random = (int) (Math.random()*13)+3;
+            int random = (int) (Math.random() * 13) + 3;
             numeros.add(random);
+            }
+            Hilo hilo1 = new Hilo("Hilo 1", numeros);
+            hilo1.start();
+
+            List<Integer> numeros2 = new ArrayList<>();
+            for (int i = 0; i < 40; i++) {
+                int random = (int) (Math.random() * 13) + 3;
+                numeros2.add(random);
+            }
+
+            Hilo thread1 = new Hilo("Hilo 1", numeros2);
+            Hilo thread2 = new Hilo("Hilo 2", numeros2);
+            Hilo thread3 = new Hilo("Hilo 3", numeros2);
+
+            thread1.start();
+            thread2.start();
+            thread3.start();
         }
-
-        Hilo hilo1 = new Hilo("Hilo 1", numeros);
-        hilo1.start();
-
-        List<Integer> numeros2 = new ArrayList<>();
-        for (int i = 0; i < 40; i++) {
-            int random = (int) (Math.random()*13)+3;
-            numeros2.add(random);
-        }
-
-        Hilo thread1 = new Hilo("Hilo 1", numeros2);
-        Hilo thread2 = new Hilo("Hilo 2", numeros2);
-        Hilo thread3 = new Hilo("Hilo 3", numeros2);
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
-    }
 }
